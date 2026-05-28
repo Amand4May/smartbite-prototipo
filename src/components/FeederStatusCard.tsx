d6bae107958e3eaa30b01fad8e13adfd8a030920
OFamosoCientista <vinicius.maxboy22@gmail.com>
2026-05-28 20:23:58 -0300

    Update FeederStatusCard.tsx

--- src/components/FeederStatusCard.tsx
+++ src/components/FeederStatusCard.tsx
@@ -27,7 +27,7 @@ export function FeederStatusCard({ status }: FeederStatusCardProps) {
       ? 'bg-[hsl(var(--warning))]'
       : 'bg-[hsl(var(--destructive))]';
 
-  const state = stateLabels[status.deviceState];
+  const state = processing; /*stateLabels[status.deviceState];*/
   const motor = motorLabels[status.motorStatus];
 
   return (
